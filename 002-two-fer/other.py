98f3797c8be35b36eb8d967ceb5629eb13484049
Mohammad Shaghouzi <sh.mohammad66@gmail.com>
2020-01-21 21:16:49 +0330

    two-fer just added

---
+++ 002-two-fer/other.py
@@ -0,0 +1,5 @@
+def two_fer(name="you"):
+    #return "One for " + name + ", one for me."
+    #return "One for %s, one for me." % name
+    #return "One for {}, one for me.".format(name)
+    return f"One for {name}, one for me."
